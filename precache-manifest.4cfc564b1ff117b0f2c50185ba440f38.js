367f1f36c1dbd9318494c09f53edf8c878faf91e
Bogdan <mormul96@ukr.net>
2020-09-13 12:11:03 +0300

    Updates

---
+++ precache-manifest.4cfc564b1ff117b0f2c50185ba440f38.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ad7bd1ad59e519e705d76c81112a405",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "f96360694c568fbf0a90",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.f47202f9.chunk.css"
+  },
+  {
+    "revision": "547ed659e89c119600f8",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.43a90774.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.43a90774.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f96360694c568fbf0a90",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.3cd1eead.chunk.js"
+  },
+  {
+    "revision": "de8179226a5696e367e9",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.c468ef2a.js"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/goit-react-hw-05-phonebook/static/media/roboto-v20-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "4570b93ae0caba2b938e2ddf091099ea",
+    "url": "/goit-react-hw-05-phonebook/static/media/roboto-v20-latin-700.4570b93a.eot"
+  },
+  {
+    "revision": "57888be7f3e68a7050452ea3157cf4de",
+    "url": "/goit-react-hw-05-phonebook/static/media/roboto-v20-latin-700.57888be7.svg"
+  },
+  {
+    "revision": "96559ffb5be917f1ce9f748bf4f34732",
+    "url": "/goit-react-hw-05-phonebook/static/media/roboto-v20-latin-700.96559ffb.ttf"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/goit-react-hw-05-phonebook/static/media/roboto-v20-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "329ae1c377b1fb667f5be6abd50327fc",
+    "url": "/goit-react-hw-05-phonebook/static/media/roboto-v20-latin-regular.329ae1c3.ttf"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/goit-react-hw-05-phonebook/static/media/roboto-v20-latin-regular.479970ff.woff2"
+  },
+  {
+    "revision": "4be1a572fca40bcb2202504cb17aed91",
+    "url": "/goit-react-hw-05-phonebook/static/media/roboto-v20-latin-regular.4be1a572.eot"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/goit-react-hw-05-phonebook/static/media/roboto-v20-latin-regular.60fa3c06.woff"
+  },
+  {
+    "revision": "8681f434273fd6a267b1a16a035c5f79",
+    "url": "/goit-react-hw-05-phonebook/static/media/roboto-v20-latin-regular.8681f434.svg"
+  }
+]);
